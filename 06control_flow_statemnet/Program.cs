1f421c8da54c1416d436af4e8dfd9add1151799e
Sodiq <sodiq7898@gmail.com>
2023-02-27 08:50:32 +0500

    Added new changes

---
+++ 06control_flow_statemnet/Program.cs
@@ -0,0 +1,81 @@
+﻿//                                                Task1
+// Console.Write("Enter first number: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Enter second number: ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+// if (firstNumber>secondNumber)
+// {
+//     Console.WriteLine("Hello World !");
+// }
+
+//                                                Task2
+
+// System.Console.Write("First number: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.Write("Second number: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// if (a==b){
+//     System.Console.WriteLine("Hello world !");
+// }
+
+//                                                Task3
+
+// Console.Write("x = ");
+// short x = Convert.ToInt16(Console.ReadLine());
+
+// Console.Write("y = ");
+// short y = Convert.ToInt16(Console.ReadLine());
+
+// if (x==y)
+// {
+//     Console.WriteLine("Yes");
+// }
+// else
+// {
+//     Console.WriteLine("No");
+// }
+
+
+//                                               Task4
+
+// Console.Write("x = ");
+// short x = Convert.ToInt16(Console.ReadLine());
+
+// Console.Write("y = ");
+// short y = Convert.ToInt16(Console.ReadLine());
+
+// if (x==y)
+// {
+//     System.Console.WriteLine(1);
+// }
+// else if (x>y)
+// {
+//     System.Console.WriteLine(2);
+// }
+// else
+// {
+//     System.Console.WriteLine(3);
+// }
+
+
+//                                               Task5
+
+// Console.Write("Enter your age: ");
+// ushort myAge = 18;
+// string result = (myAge<18) ? "Good day": "Good evening";
+
+//                                               Task6
+
+
+// Console.Write("Enter number: ");
+// long number = Convert.ToInt64(Console.ReadLine());
+
+// string result = number>=0
+//     ? "Is positive"
+//     : "Is negative";
+
+// Console.WriteLine(result);
